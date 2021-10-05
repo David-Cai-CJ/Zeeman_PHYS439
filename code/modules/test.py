f80584c41bb669c9b2c8eeed30b790bc617a8fd8
David Cai <fjfzcjj@gmail.com>
2021-10-05 03:29:02 +0000

    Added a few comments here and there.

--- code/modules/test.py
+++ code/modules/test.py
@@ -1,13 +1,3 @@
 
-import numpy
 
-def append_if_not_exists(arr, x):
-    if x not in arr:
-        arr.append(x)
-    
-def some_useless_slow_function():
-    arr = list()
-
-    for i in range(10000):
-        x = numpy.random.randint(0, 10000)
-        append_if_not_exists(arr, x)
+print("A useless file has been imported.")
