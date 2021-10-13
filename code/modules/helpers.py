16bb7def6b883977045bc023bc98c308f9da0715
David Cai <fjfzcjj@gmail.com>
2021-10-13 18:16:40 +0000

    Up to date on Oct13th.

--- code/modules/helpers.py
+++ code/modules/helpers.py
@@ -3,26 +3,25 @@ import pandas as pd
 import numpy as np
 import sys
 import os 
+from zeeman import ZeemanLab as ZL
+
+convert = ZL().convert
 
 def add_path():
     sys.path.insert(0,'/root/work/Zeeman_PHYS439/code/modules')
     
 
-def convert_vb_all(col_name = 'vb', new_col_name = 'b'):
-
+def convert_column_all(col_name = 'vh', new_col_name = 'b_mT'):
     for (dirpath, dirnames, filenames) in os.walk('data'):
-
         if 'converted' not in dirpath:
             ### we don't want to converted the files which have been converted.
             for f in filenames:
                 if f.endswith('.csv'):
-
                     _df =pd.read_csv(dirpath+os.sep+f)
+                    if col_name in _df.columns:
+                        ### CONVERSION part####
 
-                    if col_name in _df.names:
-                        ###do something
-                    ### CONVERSION part####
-                        _df[new_col_name] = _df[col_name].apply(conversion)
+                        _df[new_col_name] = _df[col_name].apply(convert)
 
                         try:
                             os.mkdir(dirpath+os.sep+'converted')
@@ -31,8 +30,67 @@ def convert_vb_all(col_name = 'vb', new_col_name = 'b'):
                         _df.to_csv(dirpath+os.sep+'converted'+os.sep+f)
 
                     else:
+                        ### Don't have the column we are looking for
                         continue
+
+
+def convert_column(file_path, col_name, new_col_name):
+        if 'converted' not in file_path:
+            ### we don't want to converted the files which have been converted.
+            if file_path.endswith('.csv'):
+                _df = pd.read_csv(file_path)
+
+                if col_name in _df.columns:
+                    _df[new_col_name] = _df[col_name].apply(convert)
+
+                    try:
+                        os.mkdir(os.path.dirname(file_path)+os.sep+'converted')
+
+                    except FileExistsError:
+                        pass
                         
+                    _df.to_csv(os.path.dirname(file_path)+os.sep+'converted'+\
+                    os.sep+os.path.basename(file_path))
+                    print('Converted successfully')
+
+                else:
+                    ### Don't have the column we are looking for
+                    print('Specified column name doesn\'t exist in the .csv file.')
+
+
+def list_files(startpath):
+    """
+    gives you a tree structure to the directory
+    """
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+
+def get_file_path(f_string):
+    possible = []
+    for (dirpath, dirnames, filenames) in os.walk('data'):
+        for f in filenames:
+            if f.endswith('.csv') and f_string in f:
+                possible.append(dirpath+os.sep+f)
+
+    if len(possible) >1:
+        print(*possible, sep = '\n')
+        idx = int(input('Out of these converted files, which do you need: '))
+
+        return possible[idx - 1]
+
+    if len(possible) == 0:
+        print('No such file found.')
+        return
+
+    if len(possible) == 1:
+        return possible[0]
+
 
 
 add_path()  ## This is what adds the modules folder to sys.path
